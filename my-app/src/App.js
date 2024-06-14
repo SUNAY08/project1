f7e86d7a7a0c05e2d8d304906fa591e2f089ba07
sunilrautela-antino <sunil.r@antino.io>
2024-06-14 23:28:59 +0530

    added NavBar

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import NavBar from './components/NavigationBar/Navbar'
+
+function App() {
+  return (
+   <div>
+      <NavBar/>
+     
+    </div>
+  );
+}
+
+export default App;
